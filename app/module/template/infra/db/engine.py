f5a1f464980c6451f52f7ec3683c47eedee12e53
D3C0RU5 <chpeterle.profissional@gmail.com>
2026-03-12 12:16:43 +0000

    chore: configure env

--- app/module/template/infra/db/engine.py
+++ app/module/template/infra/db/engine.py
@@ -1,6 +1,10 @@
 import os
 
+from dotenv import load_dotenv
+
 from app.module.shared.module.persistence.sqlalchemy.module_engine import ModuleEngine
+
+load_dotenv()
 
 DATABASE_URL = os.getenv(
     "TEMPLATE_DATABASE_URL", "postgresql+psycopg://admin:insecure@db:5432/oneclick"
